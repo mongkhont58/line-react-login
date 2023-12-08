fadb3ee848f69458eea799eb870640487cda52e0
mongkhont58 <mongkhon.t58@gmail.com>
2023-12-08 13:59:44 +0700

    html5QrCodePlugin

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import liff from '@line/liff';
 import { useEffect, useState } from 'react';
+import Html5QrcodePlugin from './Html5QrcodePlugin';
 
 function App() {
-
   const [pictureUrl, setPictureUrl] = useState(logo);
   const [idToken, setIdToken] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [statusMessage, setStatusMessage] = useState("");
   const [userId, setUserId] = useState("");
+  const [decodedBarcode, setDecodeBarcode] = useState('')
+  const [locationAccess, setLocationAccess] = useState({ lat: '', lng: '' })
+
+  useEffect(() => {
+    initLine()
+  })
 
   const logout = () => {
     liff.logout()
@@ -38,29 +44,41 @@ function App() {
       console.error(err)
       logout()
     })
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLocationAccess({ lat: position.coords.latitude, lng: position.coords.longitude })
+    })
   }
 
-  useEffect(() => {
-    initLine()
-  }, [])
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log('decodedResult', decodedResult);
+    // handle decoded results here
+    if (decodedText) {
+      setDecodeBarcode(decodedText)
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ textAlign: "center" }}>
-          <h1>React with LINE Login test bot1</h1>
-          {/* {idToken ? '' :
-            <button onClick={() => initLine()} style={{ width: "100%", height: 50 }}>Login</button>
-          } */}
+          <Html5QrcodePlugin
+            fps={10}
+            qrbox={250}
+            disableFlip={false}
+            qrCodeSuccessCallback={onNewScanResult}
+          />
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>Decoded: </b> {decodedBarcode}</p>
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>Lat: </b> {locationAccess.lat}</p>
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>Lng: </b> {locationAccess.lng}</p>
+
+          <h1>Information</h1>
           <hr />
-          <img src={pictureUrl} width="300px" height="300px" />
+          <img src={pictureUrl} width="300px" height="300px" alt='logo' />
           <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
           <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
           <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
           <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
-          {/* {idToken ? <button onClick={() => logout()} style={{ width: "100%", height: 50 }}>Logout</button>
-            : ''
-          } */}
         </div>
       </header>
     </div>
